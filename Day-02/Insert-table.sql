225ad1bec8d7a6b6e7f60ef4a43ae412b799019b
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-25 21:36:22 +0530

    Day-02 SQl Learning

---
+++ Day-02/Insert-table.sql
@@ -0,0 +1,8 @@
+Insert into Students
+(Name, ID, Age, Department, student_address)
+VALUES
+('Ajay', 101, 23, 'MCA', 'Kanigiri'),
+('Rahul', 102, 22, 'MCA', 'Vijaywada'),
+('Mani', 103, 21, 'MCA', 'Guntur'),
+('Gopi', 104, 22, 'CSE', 'KONDA'),
+('Srinu', 201, 26, 'MTECH', 'Bapatla');
